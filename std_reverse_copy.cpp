89b08d672980284cd3dabeddb8909dc999edc76b
zchengyun <1690132420@qq.com>
2021-09-29 01:33:43 +0800

    添加项目文件。

---
+++ std_reverse_copy.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> v({ 1,2,3 });
+    for (const auto& value : v) {
+        std::cout << value << " ";
+    }
+    std::cout << '\n';//1 2 3 
+
+    std::vector<int> destination(3);
+    std::reverse_copy(std::begin(v), std::end(v), std::begin(destination));
+    for (const auto& value : destination) {
+        std::cout << value << " ";
+    }
+    std::cout << '\n';//3 2 1
+}
